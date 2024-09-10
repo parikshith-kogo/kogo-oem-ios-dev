419109bdb308fbd684ca2295d1eb373ace96c5e9
Parikshith <parikshith@kogo.ai>
2024-09-10 18:09:12 +0530

    framework update

---
+++ KogoTvs.xcframework/ios-arm64/KogoTvs.framework/Headers/KogoTvs.h
@@ -0,0 +1,18 @@
+//
+//  KogoTvs.h
+//  KogoTvs
+//
+//  Created by Ankur Singh on 10/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KogoTvs.
+FOUNDATION_EXPORT double KogoTvsVersionNumber;
+
+//! Project version string for KogoTvs.
+FOUNDATION_EXPORT const unsigned char KogoTvsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KogoTvs/PublicHeader.h>
+
+
